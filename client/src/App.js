d692a265340d0a3fdf728111e85fd6a3cf75fbd8
Kygan <kygandao23@gmail.com>
2021-10-07 12:01:01 -0700

    begin, imported apollo, added mutations and query

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,38 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//Apollo imports
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+//Create Client
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id-token')
+    operation.setContext(({ headers = {} }) => ({
+      headers: {
+        ...headers,
+        authorization: localStorage.getItem('token') || null,
+      }
+    }))
+  }
+})
+
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
