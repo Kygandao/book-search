73cb446f9a5416ff9a4f3cbd9b58ee9bc07f15be
Kygan <kygandao23@gmail.com>
2021-10-07 13:45:33 -0700

    still connecting apollo server

--- server/server.js
+++ server/server.js
@@ -3,8 +3,21 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { ApolloServer } = require('apollo-server-express')
+const { auth } = require('./utils/auth')
+const { typeDefs, resolvers } = require('./schema')
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: auth
+})
+await ApolloServer.start();
+
+server.applyMiddleware({ app, path: '/' })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +29,13 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`GraphQL is running at http://localhost:${PORT}${server.graphqlPath}`)
+  })
 });
